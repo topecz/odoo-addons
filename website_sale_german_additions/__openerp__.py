43ba4a212c791dcd349498ea8b8e001dc72a2c59
ma78nee <mn@copado.de>
2014-12-18 21:00:39 +0100

    [IMP] Change to the old Version

--- website_sale_german_additions/__openerp__.py
+++ website_sale_german_additions/__openerp__.py
@@ -24,12 +24,12 @@
     'name': 'eCommerce German additions',
     'category': 'Website',
     'summary': 'German additions for eCommerce (B2C)',
-    'version': '1.0',
+    'version': '0.1',
     'description': """
 German additions for eCommerce (B2C)
 ==========================
 
-This addon installes necessary additions in accordance with statutory requirements in Germany.
+This addon installed necessary additions in accordance with statutory requirements in Germany.
 
 Overview
 --------
@@ -38,8 +38,8 @@ Overview
  - adds delivery page
  - adds privacy page
  - adds imprint page
- - adds a note on payment site, that terms and revocation are accepted with click on "Pay now"
- - adds terms and revocation popup on payment site; both are taken from terms and revocation page on the frontend
+ - adds an easy built-in address snippet to content blocks
+ - adds "incl. VAT, plus shipping costs" to every price - adds terms and revocation popup on payment site; both are taken from terms and revocation page on the frontend
 
 
 Contact for questions
@@ -47,10 +47,12 @@ Contact for questions
 copado MEDIA UG - Unterdorfstr. 29 - 77948 Friesenheim - Germany - Phone: +49 7821 32725 20 - info@copado.de - http:www.copado.de
         """,
     'author': 'copado MEDIA UG, Mathias Neef',
-    'website': 'http://www.copado.de',
     'depends': ['website_sale'],
     'data': [
-        'views/views.xml',
+        'views/wsga_views.xml',
+        'views/wsga_templates.xml',
+        'views/wsga_snippets.xml',
+        'data/data.xml',
     ],
     'demo': [],
     'installable': True,
--- website_sale_german_additions/controllers/main.py
+++ website_sale_german_additions/controllers/main.py
@@ -1,78 +1,50 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Copyright (C) 2010-2013 OpenERP s.a. (<http://openerp.com>).
-#    Copyright (C) 2014 copado MEDIA UG (<http://www.copado.de>).
-#    Author Mathias Neef <mn[at]copado.de>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 from openerp import http
 from openerp.http import request
 
-class wsat_additions(http.Controller):
+class website_sale_german_additions(http.Controller):
     @http.route(['/page/terms', '/page/website.terms'], type='http', auth='public', website=True)
-    def wsat_terms(self, **kw):
-        return http.request.render('l10n_de_website_sale_accept_terms.terms')
+    def wsga_terms(self, **kw):
+        return http.request.render('website_sale_german_additions.wsga_terms')
         
     @http.route(['/page/revocation', '/page/website.revocation'], type='http', auth='public', website=True)
-    def wsat_revocation(self, **kw):
-        return http.request.render('l10n_de_website_sale_accept_terms.revocation')
+    def wsga_revocation(self, **kw):
+        return http.request.render('website_sale_german_additions.wsga_revocation')
         
     @http.route(['/page/delivery', '/page/website.delivery'], type='http', auth='public', website=True)
-    def wsat_delivery(self, **kw):
-        return http.request.render('l10n_de_website_sale_accept_terms.delivery')
+    def wsga_delivery(self, **kw):
+        return http.request.render('website_sale_german_additions.wsga_delivery')
         
     @http.route(['/page/privacy', '/page/website.privacy'], type='http', auth='public', website=True)
-    def wsat_privacy(self, **kw):
-        return http.request.render('l10n_de_website_sale_accept_terms.privacy')
+    def wsga_privacy(self, **kw):
+        return http.request.render('website_sale_german_additions.wsga_privacy')
 
     @http.route(['/page/imprint', '/page/website.imprint'], type='http', auth='public', website=True)
-    def wsat_imprint(self, **kw):
-        return http.request.render('l10n_de_website_sale_accept_terms.imprint')
+    def wsga_privacy(self, **kw):
+        return http.request.render('website_sale_german_additions.wsga_imprint')
         
-wsat_additions()
-
-
-class wsat_popover(http.Controller):
+class wsga_popover(http.Controller):
     @http.route('/popover/terms/', auth="public", type='http')
-    def wsat_popover_terms(self, **kw):
+    def wsga_popover_terms(self, **kw):
         imd = request.registry['ir.model.data']
         iuv = request.registry['ir.ui.view']
 
-        view_id = imd.get_object_reference(request.cr, request.uid, 'l10n_de_website_sale_accept_terms', 'terms')
+        view_id = imd.get_object_reference(request.cr, request.uid, 'website_sale_german_additions', 'wsga_terms')
         view = iuv.browse(request.cr, request.uid, [(view_id[1])], context=None)
         
         xml_id = view_id[1]
         view_result = view[0].arch
         
-        return http.request.render('l10n_de_website_sale_accept_terms.popover_terms', {'html_view': view_result})
+        return http.request.render('website_sale_german_additions.wsga_popover_terms', {'html_view': view_result})
         
     @http.route('/popover/revocation/', auth="public", type='http')
-    def wsat_popover_revocation(self, **kw):
+    def wsga_popover_revocation(self, **kw):
         imd = request.registry['ir.model.data']
         iuv = request.registry['ir.ui.view']
 
-        view_id = imd.get_object_reference(request.cr, request.uid, 'l10n_de_website_sale_accept_terms', 'revocation')
+        view_id = imd.get_object_reference(request.cr, request.uid, 'website_sale_german_additions', 'wsga_revocation')
         view = iuv.browse(request.cr, request.uid, [(view_id[1])], context=None)
         
         xml_id = view_id[1]
         view_result = view[0].arch
         
-        return http.request.render('l10n_de_website_sale_accept_terms.popover_revocation', {'html_view': view_result})
-        
-wsat_popover()
+        return http.request.render('website_sale_german_additions.wsga_popover_revocation', {'html_view': view_result})
